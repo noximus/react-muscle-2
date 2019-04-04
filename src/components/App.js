2ba849ca35cb1cb72764ca2b118a89603ee1a8ea
Henry Richardson <hrichardson83@gmail.com>
2019-04-04 14:53:46 -0400

    styled components added with a bit of styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import SongList from './SongList';
-import SongDetail from './SongDetail';
+import React from "react";
+import SongList from "./SongList";
+import SongDetail from "./SongDetail";
+import styled from "styled-components";
+
+const Container = styled.div`
+  backgroundcolor: rgb(31, 11, 47);
+  margin: 30px !important;
+`;
 
 const App = () => {
   return (
-    <div className="ui container grid">
+    <Container className="ui container grid">
       <div className="ui row">
         <div className="column eight wide">
           <SongList />
@@ -13,7 +19,7 @@ const App = () => {
           <SongDetail />
         </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,35 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { selectSong } from "../actions";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: rgb(151, 75, 110) !important;
+
+  :hover {
+    background-color: rgb(213, 128, 168) !important;
+  }
+`;
+const StyledItem = styled.div`
+  border-bottom: solid 2px rgb(151, 75, 110) !important;
+  box-sizing: border !important;
+  margin-bottom: 5px !important;
+  :last-child {
+    padding-bottom: 0.21428571em !important;
+  }
+`;
 
 class SongList extends Component {
-  renderList () {
-    return this.props.songs.map((song) => {
+  renderList() {
+    return this.props.songs.map(song => {
       return (
-        <div className="item" key={song.title}>
+        <StyledItem className="item" key={song.title}>
           <div className="right floated content">
-            <button 
+            <Button
               className="ui button primary"
               onClick={() => this.props.selectSong(song)}
             >
               Select
-            </button>
+            </Button>
           </div>
           <div className="content">{song.title}</div>
-        </div>
+        </StyledItem>
       );
     });
   }
   render() {
-    return (
-      <div className="ui divided list">
-        {this.renderList()}
-      </div>
-    );
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 const mapStateToProps = state => ({
   songs: state.songs
-})
+});
 
-export default connect(mapStateToProps, { selectSong })(SongList);
+export default connect(
+  mapStateToProps,
+  { selectSong }
+)(SongList);
